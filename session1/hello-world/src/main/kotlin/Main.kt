8aaa413658917973864c62e801e80f2a7f43a099
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-05 11:37:23 +0200

    taskHelloWorldDynamicV2

--- session1/hello-world/src/main/kotlin/Main.kt
+++ session1/hello-world/src/main/kotlin/Main.kt
@@ -2,6 +2,6 @@ fun main () {
     val classmates = arrayOf("Tyron", "Sacha", "Hannelore", "Jasmien", "Jens")
     for (classmate in classmates )
     {
-        print (" Hello world " + classmate + " , ")
+        print (" Hello " + classmate + " , ")
     }
 }

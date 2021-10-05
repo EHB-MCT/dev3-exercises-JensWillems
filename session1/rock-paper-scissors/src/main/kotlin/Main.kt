e0a410869ed6ad4dad52c60e9a1900556077840e
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-05 12:20:54 +0200

    rockPaperScissorsV2

--- session1/rock-paper-scissors/src/main/kotlin/Main.kt
+++ session1/rock-paper-scissors/src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 fun main() {
+    val playerInput = readLine()
+    println("your choise :" + playerInput)
+
     val optionsGame = arrayOf("rock" , "paper" , "scissors")
     val randomNumber = (0..2).random()
     println(optionsGame[randomNumber])
+
 }

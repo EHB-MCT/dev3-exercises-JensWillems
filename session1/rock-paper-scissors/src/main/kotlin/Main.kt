b1f668ffc480606bc313491334ecf6c813e17e2e
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-05 12:59:38 +0200

    rockPaperScissorsV5

--- session1/rock-paper-scissors/src/main/kotlin/Main.kt
+++ session1/rock-paper-scissors/src/main/kotlin/Main.kt
@@ -1,13 +1,18 @@
 fun main() {
+    //player
     println("Type your answer: ")
     val playerInput = readLine()
     println("your choise: " +playerInput)
 
-    val optionsGame = arrayOf(" rock " , " paper " , " scissors ")
+    //Computer
+    val optionsGame = arrayOf("rock" , "paper" , "scissors")
     val randomNumber = (0..2).random()
     val computerChoise = optionsGame[randomNumber]
-    println("computers choise: " +computerChoise)
+    println("computers choise: " + computerChoise)
 
-
+    //Winner-loser
+    if(computerChoise == playerInput){
+        println("draw!")
+    }
 
 }

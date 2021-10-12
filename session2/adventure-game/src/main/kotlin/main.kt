463302a0a2adaee89a93ac9e265186eebfca78df
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-12 10:46:53 +0200

    session2_Stap1_Done

--- session2/adventure-game/src/main/kotlin/main.kt
+++ session2/adventure-game/src/main/kotlin/main.kt
@@ -1,5 +1,28 @@
 fun main(){
-println(
-    "line"
-)
+
+    println(
+    "U are doing your theory exam for driven"
+    )
+    println(
+        "Answer these questions to pass!"
+    )
+    challengeOne()
+}
+
+fun challengeOne(){
+    println(
+    "Your first question!, How many wheels does a car have?"
+    )
+    val riddleAnswer = arrayOf("4" , "four" , "vier")
+    println("Type your answer: ")
+    val playerAnswer = readLine()
+    if(riddleAnswer.contains(playerAnswer)){
+        println(
+            "CORRECT!"
+        )
+    } else {
+        println(
+            "U failed your exam!"
+        )
+    }
 }

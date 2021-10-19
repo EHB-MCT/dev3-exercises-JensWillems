650d5e086780f675baca2f680219f999f33ce2a6
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-19 10:56:21 +0200

    chatroulette stap 1 DONE

---
+++ session3/chatroulette/src/main/kotlin/Person.kt
@@ -0,0 +1,7 @@
+class Person(name: String) {
+    fun introduction() {
+        println(
+            "Hello,I'm how are you?"
+        )
+    }
+}
---
+++ session3/chatroulette/src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val Jack = Person("Jack")
+    val Bert = Person("Bert")
+
+    val persons = arrayOf(Jack, Bert)
+     persons.random().introduction()
+
+}

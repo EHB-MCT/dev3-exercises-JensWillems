8bfb24a3fb39d71a91d6fee6564799cc584da34e
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-10-19 09:50:12 +0200

    class course stap 1 Done

---
+++ session3/structure-class/src/main/kotlin/main.kt
@@ -0,0 +1,14 @@
+fun main (){
+    val design1 = Course("design1",
+        1,
+        5,
+        " A fine course",
+        "Maaike",
+        1,
+        "design",
+        true
+    )
+
+}
+    val design1 = Course("design1")
+}

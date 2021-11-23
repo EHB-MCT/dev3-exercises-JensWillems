d967802ba1cc3e15d28cf58d9f7ac19583c1a38c
JensWillems <71693119+JensWillems@users.noreply.github.com>
2021-11-23 12:25:39 +0100

    step 1 ,  first Querry

--- session6/publictransport/src/main/kotlin/Main.kt
+++ session6/publictransport/src/main/kotlin/Main.kt
@@ -18,5 +18,29 @@ fun main(){
                 ":" + "3306" + "/" +
                 credentials.databaseName,
         connectionProps)
+    println(
+        "Chose your destination city between Brussels or Antwerp"
+    )
+
+    val search = readLine()
+    val statement = connection.prepareStatement("SELECT * FROM S6_rides WHERE destination = ? ")
+
+    statement.setString(1, search)
+    val result = statement.executeQuery()
+    while(result.next()) {
+        println(
+            result.getString("departure_time")
+        )
+
+    }
+
+
+
+
+
+
+
+
 
 }
+
